653d51b67ff0591ad90f0e8264aeb14e0e6df2b8
bangoldx <bangold@proton.me>
2024-11-05 20:40:30 -0600

    task 3 complete

--- queries.sql
+++ queries.sql
@@ -3,5 +3,5 @@ SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.Columns WHERE TABLE_NAME =
 --Part 2
 SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
-
+DROP TABLE job;
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
@@ -41,30 +42,33 @@ public class HomeController {
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
-	model.addAttribute("title", "Add Job");
-    model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId) {
+                                    Errors errors, Model model, @RequestParam int employerId) {
 
         if (errors.hasErrors()) {
-	    model.addAttribute("title", "Add Job");
+            model.addAttribute("title", "Add Job");
             return "add";
+        } else {
+            Optional<Employer> result = employerRepository.findById(employerId);
+            Employer employer = result.get();
+            newJob.setEmployer(employer);
+            jobRepository.save(newJob);
+
+            return "redirect:";
         }
-
-        jobRepository.save(newJob);
-
-        return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
 
-            return "view";
+        return "view";
     }
 
 }
